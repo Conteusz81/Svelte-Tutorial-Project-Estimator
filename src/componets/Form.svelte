905674aa64d285a4cd9426ac9ad93b77866199f3
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-06-26 10:30:32 +0200

    got form props and events added

--- src/componets/Form.svelte
+++ src/componets/Form.svelte
@@ -1,1 +1,49 @@
-<h1>Form</h1>
+<script>
+    export let id;
+    export let name = "";
+    export let price;
+
+    $: mode = id ? "edit" : "add"
+    $: canSubmit = name !== "" && price >= 0;
+
+    function submit() {
+        if (!canSubmit) { return; }
+
+       cancel();
+    }
+
+    function cancel() {
+        price="";
+        name="";
+        id= undefined;
+    }
+</script>
+
+<style>
+    button {
+        margin-left: 20px;
+    }
+
+    button:disabled {
+        cursor: not-allowed;
+    }
+</style>
+
+<form on:submit|preventDefault={submit}>
+    <fieldset>
+        <label for="nameField">Material</label>
+        <input type="text" id="nameField" placeholder="Wood, Glue Etc." bind:value={name}/>
+
+        <label for="priceField">Price</label>
+        <input type="number" id="priceField" placeholder="Price" min="0" step="any" bind:value={price}/>
+    </fieldset>
+    <button
+       disabled={!canSubmit}
+       class="float-right"
+       type="submit">
+        { mode }
+    </button>
+    {#if mode === 'edit'}
+    <button  on:click={cancel} class="float-right" type="button">Cancel</button>
+    {/if}
+</form>
